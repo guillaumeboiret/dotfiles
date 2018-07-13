430d4106f8d888cd28ab0eb9d00c019d9af1db0e
Guillaume Boiret <guillaume@phantombuster.com>
2018-07-13 17:09:00 +0200

    zshrc secret vscode

--- setup.zsh
+++ setup.zsh
@@ -5,134 +5,138 @@ then
     rmflags="-fvr"
 fi
 
+lnflags="-sTv"
+if [ "$(uname 2> /dev/null)" != "Linux" ]; then
+    lnflags="-sv"
+fi
 mkdir ~/.config
 
-##
-# X11
-#echo "Link: ~/.Xresources -> ~/.dotfiles/x/Xresources"
-#rm $rmflags ~/.Xresources
-#ln -sTv ~/.dotfiles/x/Xresources ~/.Xresources
+## LINUX
+if [ "$(uname 2> /dev/null)" = "Linux" ]; then
+    ##
+    # X11
+    #echo "Link: ~/.Xresources -> ~/.dotfiles/x/Xresources"
+    #rm $rmflags ~/.Xresources
+    #ln $lnflags ~/.dotfiles/x/Xresources ~/.Xresources
 
-#echo "Link: ~/.xsession -> ~/.dotfiles/x/xsession"
-#rm $rmflags ~/.xsession
-#ln -sTv ~/.dotfiles/x/xsession ~/.xsession
+    #echo "Link: ~/.xsession -> ~/.dotfiles/x/xsession"
+    #rm $rmflags ~/.xsession
+    #ln $lnflags ~/.dotfiles/x/xsession ~/.xsession
 
-#echo "Link: ~/.xinitrc -> ~/.dotfiles/x/xsession"
-#rm $rmflags ~/.xinitrc
-#ln -sTv ~/.dotfiles/x/xsession ~/.xinitrc
+    #echo "Link: ~/.xinitrc -> ~/.dotfiles/x/xsession"
+    #rm $rmflags ~/.xinitrc
+    #ln $lnflags ~/.dotfiles/x/xsession ~/.xinitrc
 
-#echo "Link: ~/.Xmodmap -> ~/.dotfiles/x/Xmodmap"
-#rm $rmflags ~/.Xmodmap
-#ln -sTv ~/.dotfiles/x/Xmodmap ~/.Xmodmap
+    #echo "Link: ~/.Xmodmap -> ~/.dotfiles/x/Xmodmap"
+    #rm $rmflags ~/.Xmodmap
+    #ln $lnflags ~/.dotfiles/x/Xmodmap ~/.Xmodmap
 
-# pentadactyl
-#echo "Link: ~/.pentadactylrc -> ~/.dotfiles/pentadactyl/pentadactylrc"
-#rm $rmflags ~/.pentadactylrc
-#ln -sTv ~/.dotfiles/pentadactyl/pentadactylrc ~/.pentadactylrc
+    # pentadactyl
+    #echo "Link: ~/.pentadactylrc -> ~/.dotfiles/pentadactyl/pentadactylrc"
+    #rm $rmflags ~/.pentadactylrc
+    #ln $lnflags ~/.dotfiles/pentadactyl/pentadactylrc ~/.pentadactylrc
 
-#echo "Link: ~/.pentadactyl -> ~/.dotfiles/pentadactyl/pentadactyl"
-#rm $rmflags ~/.pentadactyl
-#ln -sTv ~/.dotfiles/pentadactyl/pentadactyl ~/.pentadactyl
+    #echo "Link: ~/.pentadactyl -> ~/.dotfiles/pentadactyl/pentadactyl"
+    #rm $rmflags ~/.pentadactyl
+    #ln $lnflags ~/.dotfiles/pentadactyl/pentadactyl ~/.pentadactyl
 
-# icons
-rm $rmflags ~/.icons
-ln -sTv ~/.dotfiles/icons ~/.icons
+    # icons
+    rm $rmflags ~/.icons
+    ln $lnflags ~/.dotfiles/icons ~/.icons
 
-# fonts
-#echo "Link: ~/.fonts.conf -> ~/.dotfiles/fonts/fonts.conf"
-#rm $rmflags ~/.fonts.conf
-#ln -sTv ~/.dotfiles/fonts/fonts.conf ~/.fonts.conf
+    # fonts
+    #echo "Link: ~/.fonts.conf -> ~/.dotfiles/fonts/fonts.conf"
+    #rm $rmflags ~/.fonts.conf
+    #ln $lnflags ~/.dotfiles/fonts/fonts.conf ~/.fonts.conf
 
-rm $rmflags ~/.fonts
-ln -sTv ~/.dotfiles/fonts ~/.fonts
+    rm $rmflags ~/.fonts
+    ln $lnflags ~/.dotfiles/fonts ~/.fonts
 
-# gsimplecal
-#mkdir -p ~/.config/gsimplecal
+    # gsimplecal
+    #mkdir -p ~/.config/gsimplecal
 
-#echo "Link: ~/.config/gsimplecal/config -> ~/.dotfiles/gsimplecal/config"
-#rm $rmflags ~/.config/gsimplecal/config
-#ln -sTv ~/.dotfiles/gsimplecal/config ~/.config/gsimplecal/config
+    #echo "Link: ~/.config/gsimplecal/config -> ~/.dotfiles/gsimplecal/config"
+    #rm $rmflags ~/.config/gsimplecal/config
+    #ln $lnflags ~/.dotfiles/gsimplecal/config ~/.config/gsimplecal/config
 
-# parcellite
-#mkdir -p ~/.config/parcellite
+    # parcellite
+    #mkdir -p ~/.config/parcellite
 
-#echo "Link: ~/.config/parcellite/parcelliterc -> ~/.dotfiles/parcellite/parcelliterc"
-#rm $rmflags ~/.config/parcellite/parcelliterc
-#ln -sTv ~/.dotfiles/parcellite/parcelliterc ~/.config/parcellite/parcelliterc
+    #echo "Link: ~/.config/parcellite/parcelliterc -> ~/.dotfiles/parcellite/parcelliterc"
+    #rm $rmflags ~/.config/parcellite/parcelliterc
+    #ln $lnflags ~/.dotfiles/parcellite/parcelliterc ~/.config/parcellite/parcelliterc
+
+    # nvim
+    rm $rmflags ~/.config/nvim
+    ln $lnflags ~/.dotfiles/nvim ~/.config/nvim
+
+    #echo "Link: ~/.inputrc -> ~/.dotfiles/zsh/inputrc"
+    #rm $rmflags ~/.inputrc
+    #ln $lnflags ~/.dotfiles/zsh/inputrc ~/.inputrc
+
+    # htop
+    mkdir -p ~/.config/htop
+    rm $rmflags ~/.config/htop/htoprc
+    ln $lnflags ~/.dotfiles/htop/htoprc ~/.config/htop/htoprc
+
+    # i3
+    rm $rmflags ~/.config/i3
+    ln $lnflags ~/.dotfiles/i3 ~/.config/i3
+
+    # screen layout
+    rm $rmflags ~/.screenlayout
+    ln $lnflags ~/.dotfiles/screenlayout ~/.screenlayout
+
+    # screen layout
+    rm $rmflags ~/.config/compton.conf
+    ln $lnflags ~/.dotfiles/compton.conf ~/.config/compton.conf
+
+    # feh
+    rm $rmflags ~/.fehbg
+    ln $lnflags ~/.dotfiles/fehbg ~/.fehbg
+
+    # Xresources
+    rm $rmflags ~/.Xresources
+    ln $lnflags ~/.dotfiles/Xresources ~/.Xresources
+
+    # Xinit
+    rm $rmflags ~/.xinitrc
+    ln $lnflags ~/.dotfiles/xinitrc ~/.xinitrc
+fi
 
 # vim
 #echo "Link: ~/.vimrc -> ~/.dotfiles/vim/vimrc"
 #rm $rmflags ~/.vimrc
-#ln -sTv ~/.dotfiles/vim/vimrc ~/.vimrc
+#ln $lnflags ~/.dotfiles/vim/vimrc ~/.vimrc
 
 #echo "Link: ~/.gvimrc -> ~/.dotfiles/vim/gvimrc"
 #rm $rmflags ~/.gvimrc
-#ln -sTv ~/.dotfiles/vim/gvimrc ~/.gvimrc
+#ln $lnflags ~/.dotfiles/vim/gvimrc ~/.gvimrc
 
 #echo "Link: ~/.vim -> ~/.dotfiles/vim/vim"
 #rm $rmflags ~/.vim
-#ln -sTv ~/.dotfiles/vim/vim ~/.vim
-
-# nvim
-rm $rmflags ~/.config/nvim
-ln -sTv ~/.dotfiles/nvim ~/.config/nvim
+#ln $lnflags ~/.dotfiles/vim/vim ~/.vim
 
 # zsh
 rm $rmflags ~/.zshrc
-ln -sTv ~/.dotfiles/zsh/zshrc.debian ~/.zshrc
+rm $rmflags ~/.zshrc
+ln $lnflags ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 #echo "Link: ~/.zshenv -> ~/.dotfiles/zsh/zshenv"
 #rm $rmflags ~/.zshenv
-#ln -sTv ~/.dotfiles/zsh/zshenv ~/.zshenv
-
-#echo "Link: ~/.inputrc -> ~/.dotfiles/zsh/inputrc"
-#rm $rmflags ~/.inputrc
-#ln -sTv ~/.dotfiles/zsh/inputrc ~/.inputrc
+#ln $lnflags ~/.dotfiles/zsh/zshenv ~/.zshenv
 
 # git
 rm $rmflags ~/.gitconfig
-ln -sTv ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln $lnflags ~/.dotfiles/git/gitconfig ~/.gitconfig
 
 # tmux
 #echo "Link: ~/.tmux.conf -> ~/.dotfiles/tmux/tmux.conf"
 #rm $rmflags ~/.tmux.conf
-#ln -sTv ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-
-# htop
-mkdir -p ~/.config/htop
-rm $rmflags ~/.config/htop/htoprc
-ln -sTv ~/.dotfiles/htop/htoprc ~/.config/htop/htoprc
-
-# i3
-rm $rmflags ~/.config/i3
-ln -sTv ~/.dotfiles/i3 ~/.config/i3
-
-# screen layout
-rm $rmflags ~/.screenlayout
-ln -sTv ~/.dotfiles/screenlayout ~/.screenlayout
-
-# screen layout
-rm $rmflags ~/.config/compton.conf
-ln -sTv ~/.dotfiles/compton.conf ~/.config/compton.conf
-
-# feh
-rm $rmflags ~/.fehbg
-ln -sTv ~/.dotfiles/fehbg ~/.fehbg
-
-# export
-#rm $rmflags ~/.export.zsh
-#ln -sTv ~/.dotfiles/zsh/export.zsh ~/.export.zsh
+#ln $lnflags ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # vscode macOS
 rm $rmflags ~/Library/Application\ Support/Code/User/snippets
 rm $rmflags ~/Library/Application\ Support/Code/User/settings.json
-ln -sv ~/.dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/
-ln -sv ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-
-# Xresources
-rm $rmflags ~/.Xresources
-ln -sTv ~/.dotfiles/Xresources ~/.Xresources
-
-# Xinit
-rm $rmflags ~/.xinitrc
-ln -sTv ~/.dotfiles/xinitrc ~/.xinitrc
+ln $lnflags ~/.dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/
+ln $lnflags ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
