b9466feac063f0d32b76c131d7c4ca0f363dc9c6
guillaumeboiret <guillaume.boiret@gmail.com>
2019-05-08 23:38:01 +0200

    sorted by config and home files

--- setup.zsh
+++ setup.zsh
@@ -5,21 +5,22 @@ LN_FLAGS="sT"
 
 typeset -A CONFIG_FILE;
 CONFIG_FILE[zsh/zshrc]=".zshrc"
+# Home
 CONFIG_FILE[git/gitconfig]=".gitconfig"
 CONFIG_FILE[icons]=".icons"
 CONFIG_FILE[fonts]=".fonts"
-CONFIG_FILE[nvim]=".config/nvim"
-CONFIG_FILE[zsh/inputrc]=".inputrc"
-CONFIG_FILE[htop/htoprc]=".config/htop/htoprc"
-CONFIG_FILE[i3/config]=".config/i3/config"
-CONFIG_FILE[polybar]=".config/polybar"
 CONFIG_FILE[screenlayout]=".screenlayout"
-CONFIG_FILE[compton/config]=".config/compton.conf"
 CONFIG_FILE[fehbg]=".fehbg"
 CONFIG_FILE[Xresources]=".Xresources"
 CONFIG_FILE[xinitrc]=".xinitrc"
-CONFIG_FILE[zsh/zshrc]=".zshrc"
 #CONFIG_FILE[vim/vimrc]=".vimrc"
+CONFIG_FILE[zsh/zshrc]=".zshrc"
+
+# Config
+CONFIG_FILE[htop]=".config/htop"
+CONFIG_FILE[i3]=".config/i3"
+CONFIG_FILE[polybar]=".config/polybar"
+CONFIG_FILE[compton]=".config/compton"
 CONFIG_FILE[gtk-3.0]=".config/gtk-3.0"
 
 echo "-> Removing existing files..."
